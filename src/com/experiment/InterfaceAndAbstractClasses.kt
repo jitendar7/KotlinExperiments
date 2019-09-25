62981197224ba41fb80c72d4d6b895b8b89807b0
Jitendar Mandali <jitendar7@gmail.com>
2019-09-25 20:29:48 +0400

    Interface , Abstract class

---
+++ src/com/experiment/InterfaceAndAbstractClasses.kt
@@ -0,0 +1,35 @@
+package com.experiment
+
+//Defining interfaces, abstract class & implementing the same
+
+interface TestInt {
+    var xyz: Int
+    val diceCount: Int
+        get() = (0 until 10).random()   //default property getters in implementation
+
+    fun attack(): Int {      //default function implementation
+        return 0
+    }
+}
+
+abstract class Villan : TestInt {  //notice there is no '( )' after TestInt
+    abstract fun defence()
+
+    override fun attack(): Int {
+        println("Villan attack")
+        return super.attack()       //super keyword used
+    }
+}
+
+class Monster(override var xyz: Int) : Villan() {       //notice the override property variable
+    override fun defence() {        //override keyword used for the function
+        println("Defence - $xyz")   //string template
+    }
+
+}
+
+fun main() {
+    val monster = Monster(23)
+    monster.defence()
+    monster.attack()
+}
