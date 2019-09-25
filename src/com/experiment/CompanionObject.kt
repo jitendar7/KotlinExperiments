77668c38887985d238bdecbbf11c02e500b447bc
Jitendar Mandali <jitendar7@gmail.com>
2019-09-25 21:16:02 +0400

    object, companion object

--- src/com/experiment/CompanionObject.kt
+++ src/com/experiment/CompanionObject.kt
@@ -1,23 +1,44 @@
 package com.experiment
 
-
-//Companion objects are defined for 'static constants, static methods' just like in java
-
 const val yellow = "Aiasdk"
 
-fun main(args: Array<String>) {
-
-    Foo.test()
-
-
+class Game {
+    init {
+        println("New Game created")
+    }
 }
 
-class Foo {
-    companion object Hello{
+// Singleton - declare the class as usual way, but use the
+// keyword 'object' instead of 'class'
+object GameFactory {
+    var game = mutableListOf<Game>()
 
+    fun makeGame() {
+        println("Make a Game")
+    }
+}
+
+//Companion object created when u need to tie a function or property to the 'class'
+//rather than the instance of it
+
+//Companion objects are defined for 'static constants / static methods' just like in java
+
+class Foo {
+    companion object Hello {
         @JvmStatic
         fun test() {
             println("This is rocking")
         }
     }
+}
+
+//Main Function
+fun main(args: Array<String>) {
+
+    //object call
+    GameFactory.makeGame()
+    GameFactory.game
+
+    //companion object call
+    Foo.test()
 }
--- src/com/experiment/InterfaceAndAbstractClasses.kt
+++ src/com/experiment/InterfaceAndAbstractClasses.kt
@@ -7,17 +7,17 @@ interface TestInt {
     val diceCount: Int
         get() = (0 until 10).random()   //default property getters in implementation
 
-    fun attack(): Int {      //default function implementation
+    fun attack(): Int {                 //default function implementation
         return 0
     }
 }
 
-abstract class Villan : TestInt {  //notice there is no '( )' after TestInt
+abstract class Villan : TestInt {       //notice there is no '( )' after TestInt
     abstract fun defence()
 
     override fun attack(): Int {
         println("Villan attack")
-        return super.attack()       //super keyword used
+        return super.attack()           //super keyword used
     }
 }
 
@@ -25,7 +25,6 @@ class Monster(override var xyz: Int) : Villan() {       //notice the override pr
     override fun defence() {        //override keyword used for the function
         println("Defence - $xyz")   //string template
     }
-
 }
 
 fun main() {
