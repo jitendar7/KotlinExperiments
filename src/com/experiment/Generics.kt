0bdb469faf176bbd2529508bc00314927040d2a2
Jitendar Mandali <jitendar7@gmail.com>
2019-09-28 12:00:19 +0400

    completed Generics

--- src/com/experiment/Generics.kt
+++ src/com/experiment/Generics.kt
@@ -3,30 +3,86 @@ package com.experiment
 //Generics
 
 
-//generic type
-class Loot<T>(var loot: T) {  //notice the class name has <T> defined, otherwise it's a compilation error
+//Generic type
+class LootBox<T : Loot>(vararg item: T) {  //notice the class name has <T> defined, otherwise it's a compilation error
+    //also notice that <T: Loot> will allow only classes which extend Loot class
     var open = false
 
+    private var loot: Array<out T> = item   //notice the <out T> defined
+
     //generic function
-    fun fetch(): T? {
-        return loot.takeIf { open }
+    fun fetch(item: Int): T? {
+        return loot[item].takeIf { open }
+    }
+
+    //multiple generic parameters
+    fun <P> fetch(modFunction: (P)) {       //notice the '<P>' parameter defined after fun
+
+    }
+
+    fun <R> fetch(item: Int, lootModFunction: (T) -> R): R? {   //notice only '<R>' parameter defined after fun
+        return lootModFunction(loot[item]).takeIf { open }
+    }
+}
+
+open class Loot(val value: Int) //Generic class, which is now restricting LootBox
+
+class Fedora(val name: String, value: Int) : Loot(value)
+
+class Coin(value: Int) : Loot(value)
+
+
+//producer, consumer geric types
+//producer => readable (out) => defined as 'val' , eg: Lists are producers
+//consumer => writable (in)
+class Barrel1<out T>(val item: T) // notice that 'out', 'val' , producer
+
+class Barrel2<in T>(item: T)        //notice that 'in', NO 'val'/'var' , consumer
+
+
+inline fun <reified T>       // 'reified' keyword used for preserving type information at runtime
+        randomOrBackupLoot(backupLoot: () -> T): T {
+    val items = listOf(Coin(14), Fedora("a fedora", 150))
+    val first: Loot = items.shuffled().first()
+    return if (first is T) {        //notice this
+        first
+    } else {
+        backupLoot()
     }
 }
 
 
-
-
-class Fedora(val name: String, val value: Int)
-
-class Coin(val value: Int)
-
-
 fun main() {
-    val lootBoxOne: Loot<Fedora> = Loot(Fedora("a generic-looking fedora", 15))
-    val lootBoxTwo: Loot<Coin> = Loot(Coin(12))
+    //notice the <Loot> is the type defined for 'LootBox'
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
+    val lootBoxTwo: LootBox<Loot> = LootBox(Coin(12))
 
-    lootBoxOne.fetch()?.run {               //'run' returning the last statement
+    //Below is no longer valid
+    // val Coin: coin = lootBoxTwo.loot
+
+    lootBoxOne.open = true
+    lootBoxOne.fetch(0)?.run {
+        //'run' returning the last statement
         println("You retrieve $name from the box!")
     }
+
+    val coin = lootBoxOne.fetch(0) {
+        Coin(it.value * 3)
+    }
+    coin?.let { println(it.value) }
+
+
+    var fedoraBarrel: Barrel1<Fedora> = Barrel1(Fedora("a generic-looking fedora", 15))
+    var lootBarrel: Barrel1<Loot>
+
+    lootBarrel = fedoraBarrel       // this is possible only bcz it is 'out'
+    val myFedora = lootBarrel.item  // smart casted
+
+    var fedoraBarrel2: Barrel2<Fedora> = Barrel2(Fedora("a generic-looking fedora", 15))
+    var lootBarrel2: Barrel2<Loot> = Barrel2(Coin(19))
+
+    //lootBarrel2 = fedoraBarrel2       //this is not possible only bcz it it 'in'
+    fedoraBarrel2 = lootBarrel2
+
 
 }
